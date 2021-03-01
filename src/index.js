7191b5b472a3857833e0dda9853fc786f9d751e0
Kiryl Krasouski <ziksow@gmail.com>
2021-03-01 21:42:25 +0300

    init: add brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let couples = [];
+  for (i = 0; i < bracketsConfig.length; i++) {
+    couples.push(bracketsConfig[i].join(''))
+  }
+  for (let i = 0; i < couples.length; i++) {
+    if (str.includes(couples[i])) {
+      str = str.replace(couples[i], '');
+      i = -1;
+    }
+  }
+  return str.length === 0 ? true : false;
+
 }
